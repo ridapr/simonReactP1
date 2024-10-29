f3b1e16beec2cb188f3b8de1de4d88ea184af02e
Mark Clement <clement@cs.byu.edu>
2024-10-28 18:50:07 -0600

    With Routes

--- src/app.jsx
+++ src/app.jsx
@@ -40,7 +40,13 @@ export default function App() {
 	</nav>
       </header>
 
-      <main>App components go here</main>
+      <Routes>
+	<Route path='/' element={<Login />} exact />
+	<Route path='/play' element={<Play />} />
+	<Route path='/scores' element={<Scores />} />
+	<Route path='/about' element={<About />} />
+	<Route path='*' element={<NotFound />} />
+      </Routes>
 
       <footer className='bg-dark text-white-50'>
         <div className='container-fluid'>
@@ -53,4 +59,8 @@ export default function App() {
     </div>
     </BrowserRouter>
   );
+}
+
+function NotFound() {
+  return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
 }
